e0536580068eeeb88b7093e8ebc4e4da8489a429
wijaya <danangwijaya750@gmail.com>
2021-03-20 12:21:27 +0700

    create main menu and select hero

---
+++ Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour
+{
+    public GameObject panelGameSettings;
+
+    [SerializeField]
+    private SessionSettings session = null;
+
+    [SerializeField]
+    private SessionSettings defaultSettings = null;
+
+    private void Awake(){
+        if(defaultSettings!=null){
+            session.Player1Character=defaultSettings.Player1Character;
+            session.Player2Character=defaultSettings.Player2Character;
+        }    
+    }
+
+   public void buttonPlayClick(){
+       panelGameSettings.SetActive(true);
+   }
+   public void buttonQuitClick(){
+
+   }
+   public void SetP1Character(int character){
+       session.Player1Character=character;
+   }
+   public void SetP2Character(int character){
+       session.Player2Character=character;
+   }
+   public void closePanel(){
+       panelGameSettings.SetActive(false);
+   }
+   public void startGame(){
+    
+   }
+}
---
+++ Assets/Scripts/SessionSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Settings", menuName = "Game Session Settings")]
+public class SessionSettings : ScriptableObject
+{
+    public int Player1Character { get => p1Character; set => p1Character = value; }
+    public int Player2Character { get => p2Character; set => p2Character = value; }
+
+    [SerializeField]
+    [Min(0)]
+    private int p1Character = 0;
+
+    [SerializeField]
+    [Min(0)]
+    private int p2Character = 1;
+}
