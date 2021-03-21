b2540cfffc56e94adbd44f5d3154b34bbc635601
mfaizudd <mfaizudd@gmail.com>
2021-03-21 21:31:44 +0700

    Add multiplayer support

---
+++ Assets/Scripts/ControlMap/Player2Control.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine.InputSystem;
+using ControlMap;
+
+public class Player2Control : IPlayerControl, Player2InputAction.IGameplayActions
+{
+    public IInputActionCollection InputAction => inputAction;
+    Player2InputAction inputAction;
+    
+    public Player2Control()
+    {
+        inputAction = new Player2InputAction();
+        inputAction.Gameplay.SetCallbacks(this);
+    }
+
+    public event Action<InputAction.CallbackContext> Move;
+    public event Action<InputAction.CallbackContext> Attack;
+
+    public void Enable()
+    {
+        inputAction.Enable();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        Attack?.Invoke(context);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Move?.Invoke(context);
+    }
+}
